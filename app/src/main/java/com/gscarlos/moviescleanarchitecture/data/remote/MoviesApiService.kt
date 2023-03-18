83c8bb6d60d2a6116b97a638c0b63462f7b5d276
Carlos Galindo <gscarlos1010@gmail.com>
2023-03-18 17:05:07 -0600

    Configuracion de retrofit con hilt

---
+++ app/src/main/java/com/gscarlos/moviescleanarchitecture/data/remote/MoviesApiService.kt
@@ -0,0 +1,15 @@
+package com.gscarlos.moviescleanarchitecture.data.remote
+
+import com.gscarlos.moviescleanarchitecture.data.remote.responses.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface MoviesApiService {
+
+    @GET("discover/movie")
+    suspend fun getMovies(
+        @Query("api_key") apiKey: String
+    ): Response<MovieResponse>
+}
---
+++ app/src/main/java/com/gscarlos/moviescleanarchitecture/di/ApiModule.kt
@@ -0,0 +1,26 @@
+package com.gscarlos.moviescleanarchitecture.di
+
+import com.gscarlos.moviescleanarchitecture.data.remote.MoviesApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideApiService(): MoviesApiService {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MoviesApiService::class.java)
+    }
+}
