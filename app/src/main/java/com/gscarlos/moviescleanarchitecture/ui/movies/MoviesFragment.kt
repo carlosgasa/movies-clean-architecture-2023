7d083a0b5609eb73b32600135186065f79d25874
carlosgasa <carlos.oficial.uaz@gmail.com>
2023-03-18 16:36:58 -0600

    Se agrega bottom y todos los fragments

---
+++ app/src/main/java/com/gscarlos/moviescleanarchitecture/ui/movies/MoviesFragment.kt
@@ -0,0 +1,28 @@
+package com.gscarlos.moviescleanarchitecture.ui.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gscarlos.moviescleanarchitecture.databinding.FragmentMoviesBinding
+
+class MoviesFragment : Fragment() {
+
+    private var _binding: FragmentMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/gscarlos/moviescleanarchitecture/ui/movies/MoviesViewModel.kt
@@ -0,0 +1,10 @@
+package com.gscarlos.moviescleanarchitecture.ui.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MoviesViewModel : ViewModel() {
+
+
+}
